f55dfc185a579f4d059bddb96fdda8e70440889b
mouhammadAlAli <mohamam.ali.alkhabbaz@gmail.com>
2023-09-05 14:31:45 +0300

    local files

---
+++ ArabianCoBackend/src/ArabianCo.Core/Localization/ArabianCoLocalizationConfigurer.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Configuration.Startup;
+using Abp.Localization.Dictionaries;
+using Abp.Localization.Dictionaries.Xml;
+using Abp.Reflection.Extensions;
+
+namespace ArabianCo.Localization
+{
+    public static class ArabianCoLocalizationConfigurer
+    {
+        public static void Configure(ILocalizationConfiguration localizationConfiguration)
+        {
+            localizationConfiguration.Sources.Add(
+                new DictionaryBasedLocalizationSource(ArabianCoConsts.LocalizationSourceName,
+                    new XmlEmbeddedFileLocalizationDictionaryProvider(
+                        typeof(ArabianCoLocalizationConfigurer).GetAssembly(),
+                        "ArabianCo.Localization.SourceFiles"
+                    )
+                )
+            );
+        }
+    }
+}
