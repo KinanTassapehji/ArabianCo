1393c9e1bd18d36ac9f5e7ecbbaf048bd68f4d69
mouhammadAlAli <mohamam.ali.alkhabbaz@gmail.com>
2023-10-21 14:50:37 +0300

    commit

--- ArabianCoBackend/src/ArabianCo.Application/Brands/Dto/BrandDto.cs
+++ ArabianCoBackend/src/ArabianCo.Application/Brands/Dto/BrandDto.cs
@@ -1,6 +1,5 @@
 ﻿using Abp.Application.Services.Dto;
 using ArabianCo.Attachments.Dto;
-using ArabianCo.Domain.Brands;
 using System.Collections.Generic;
 
 namespace ArabianCo.Brands.Dto;
@@ -9,6 +8,6 @@ public class BrandDto:EntityDto
 {
     public string Name { get; set; }
     public string Description { get; set; }
-    public List<BrandTranslation> Translations { get; set; }
+    public List<BrandTranslationDto> Translations { get; set; }
     public LiteAttachmentDto Photo { get; set; }
 }
