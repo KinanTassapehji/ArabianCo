749cb0617dae2741581d0c5d7a57844be11150c2
user <waleedkhaled1994@hotmail.com>
2025-05-27 22:11:03 +0300

    add ignored file

---
+++ ArabianCoBackend/src/ArabianCo.Web.Host/Startup/DefaultServiceProviderIsService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ArabianCo.Web.Host.Startup
+{
+    internal class DefaultServiceProviderIsService : IServiceProviderIsService
+    {
+        private readonly IServiceCollection _services;
+
+        public DefaultServiceProviderIsService(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        public bool IsService(Type serviceType)
+        {
+            foreach (var descriptor in _services)
+            {
+                if (descriptor.ServiceType == serviceType)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
